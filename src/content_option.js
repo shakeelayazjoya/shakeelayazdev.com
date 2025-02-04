f657d00f3a6cf1893ae513b3894f5d0514969d7f
shakeelayazjoya <shakeelayaz954@gmail.com>
2025-02-04 12:53:57 +0500

    my portfolio code

---
+++ src/content_option.js
@@ -0,0 +1,200 @@
+const logotext = "Shakeel";
+const meta = {
+  title: "Shakeel Ayaz",
+  description:
+    "I’m Shakeel Ayaz data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+  title: "I’m Shakeel Ayaz",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I am React Developer",
+  },
+  description:
+    "I am a Mern  stack developer with 3 years of experience in web development. I have a passion for learning and sharing my knowledge with others a public speaker and a teacher. I am a self-taught developer and believe in the power of community and open-source.",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "I am a Mern  stack developer with 3 years of experience in web development. I have a passion for learning and sharing my knowledge with others a public speaker and a teacher. I am a self-taught developer and believe in the power of community and open-source.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Frontend Developer ",
+    where: "maxcoretechnologies",
+    date: "2022",
+  },
+  {
+    jobtitle: "React Developer",
+    where: "maxcoretechnologies",
+    date: "2023",
+  },
+  {
+    jobtitle: "Mern Stack Developer",
+    where: "softsuite technology",
+    date: "2024",
+  },
+];
+
+const skills = [
+  {
+    name: "React js ",
+    value: 90,
+  },
+  {
+    name: "Node js",
+    value: 85,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "Next js",
+    value: 60,
+  },
+  {
+    name: "HTML & Tailwind css ",
+    value: 85,
+  },
+];
+
+const services = [
+  {
+    title: "Frontend Development",
+    description:
+      "I can develop your website in react js with the best performance and best user experience.",
+  },
+  {
+    title: "React js Development and Next js Development ",
+    description:
+      "I can develop your website in react js and next js with the best performance and best user experience.",
+  },
+  {
+    title: "Mern Stack Development",
+    description:
+      "I can develop your website in mern stack development with the best performance and best user experience.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8RzQuM0NwMayC9xWWBONM9KmFu8SU-pXyTw&s",
+    description:
+      "Task Plus website is about team management and task management.",
+    link: "taskplus.io",
+    target: "_blank",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+    target: "_blank",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+    target: "_blank",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+    target: "_blank",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+    target: "_blank",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+    target: "_blank",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+    target: "_blank",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+    target: "_blank",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+    target: "_blank",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+    target: "_blank",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+    target: "_blank",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+    target: "_blank",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "shakeelayaz954@gmail.com",
+  YOUR_FONE: "(303)2991954",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/shakeelayazjoya",
+  facebook: "https://www.facebook.com/muhammad.shakeelayaz",
+  linkedin: "https://www.linkedin.com/in/muhammad-shakeel-ayaz-37883b24b/",
+  instagram: "https://www.instagram.com/muhammadshakeelayaz/",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
